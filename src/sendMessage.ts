3f20d8a3ee6ad60e1d417fd0bd3ec9b359e23650
Jason Hurst <gorgano@gmail.com>
2023-10-27 13:45:36 -0700

    Iterate over send methods

--- src/sendMessage.ts
+++ src/sendMessage.ts
@@ -5,55 +5,67 @@ import { getTypedError } from './lib/utils';
 import { definedEnv } from './env';
 
 class UnableToSend extends Error {
-	primaryError: Error | string;
-	secondaryError: Error | string;
-	constructor(message: string, primaryError: Error | string, secondaryError: Error | string) {
+	sendErrors: (Error | string)[];
+	constructor(message: string, sendErrors: (Error | string)[]) {
 		super(message);
-		this.primaryError = primaryError;
-		this.secondaryError = secondaryError;
+		this.sendErrors = sendErrors;
 	}
 }
 
-
-const primary = SendTypes.mailgun;
-const secondary = SendTypes.sendgrid;
+// Methods to try a send with
+// Any new methods added will be automatcially add to the send chain
 const sendFunctions = {
 	[SendTypes.mailgun]: sendMailGun,
 	[SendTypes.sendgrid]: sendSendGrid,
 };
 
-// const getSendOrder = (): SendTypes[] => {
-// 	const sendOrder: SendTypes[] = [];
+/**
+ * Get an array of email provides to try.  Reads from
+ * parameters to get prefered primary, but selects on if none is
+ * present.
+ * @returns Array with order to try email providers in
+ */
+const getSendOrder = (): SendTypes[] => {
+	const sendOrder: SendTypes[] = [];
 
-// 	const requestedPrimary = env.primarySend;
-// 	if (Object.keys(SendTypes).includes(requestedPrimary)) { sendOrder.push(requestedPrimary); }
+	const requestedPrimary = definedEnv.primarySend;
+	if (Object.keys(SendTypes).includes(requestedPrimary)) {
+		sendOrder.push(requestedPrimary as SendTypes);
+	}
 
-// 	// Populate other types that aren't requested primary
-// 	// When no requested primary, this will 'pick' a primary
-// 	Object.keys(SendTypes).forEach((sendType: SendTypes) => {
-// 		if (!sendOrder.includes(sendType)) { sendOrder.push(sendType); }
-// 	});
+	// Populate other types that aren't requested primary
+	// When no requested primary, this will 'pick' a primary
+	Object.keys(SendTypes).forEach((sendType: string) => {
+		if (!sendOrder.includes(sendType as SendTypes)) {
+			sendOrder.push(sendType as SendTypes);
+		}
+	});
 
-// 	return sendOrder;
-// }
+	return sendOrder;
+}
 
 export const sendMessage = async (messageDetails: SendMessage) => {
-	// const sendOrder = getSendOrder();
-	try {
-		// await sendSendGrid(messageDetails);
-		await sendFunctions[primary](messageDetails);
-	} catch (primaryError) {
-		// Logger.error(...)
-		console.log(`sendMessage: Primary: FAIL ${primaryError}`);
+	const sendOrder = getSendOrder();
+
+	const errLog: (Error | string)[] = [];
+	let sent = false;
+
+	// Try all send method until one is successful.
+	for (let i = 0; i < sendOrder.length; ++i) {
 		try {
-			// await sendMailGun(messageDetails);
-			await sendFunctions[secondary](messageDetails);
-		} catch (secondaryError) {
-			// Logger.error
-			console.log(`sendMessage: Primary: FAIL ${secondaryError}`);
-			const pErr = getTypedError(primaryError);
-			const sErr = getTypedError(secondaryError);
-			throw new UnableToSend('Unable to send email', pErr, sErr);
+			// await sendSendGrid(messageDetails);
+			const sendType = sendOrder[i];
+			await sendFunctions[sendType](messageDetails);
+			sent = true;
+			break;
+		} catch (e) {
+			// Logger.error(...)
+			console.log(`sendMessage: ${i}/${sendOrder.length - 1} ${sendOrder[i]}: FAIL ${e}`);
+			errLog.push(getTypedError(e));
 		}
 	}
+
+	if (sent) return;
+
+	throw new UnableToSend('Unable to send email', errLog);
 }
